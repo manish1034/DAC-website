082766bfaf4cdf39fdea2a5e9dbebad44a949a81
manish1034 <76244041+manish1034@users.noreply.github.com>
2023-11-02 23:29:22 +0530

    today's commit

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -4,15 +4,15 @@ import { us } from "../assets";
 const Homepage = () => {
   return (
     <div className="h-[100vh] w-[100vw] bg-hero-site">
-      <div className="flex h-[100vh] w-[100vw] bg-black bg-opacity-90">
+      <div className="flex h-[100vh] w-[100vw] bg-black opacity-90">
         {/* Sidebar */}
         <Sidebar/>
-        <div className="text-white">
+        <div className="text-white flex-1">
           safdfas
         </div>
-        <img src={us} alt="" className="absolute h-[100vh] right-0"/>
+        <img src={us} alt="" className="relative h-[100vh] right-0 object-cover clip-photo z-10"/>
       </div>
-    </div>  
+    </div> 
   )
 }
 
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,7 @@ import { FaHome } from "react-icons/fa";
 
 const Sidebar = () => {
   return (
-    <div className="h-full flex flex-col justify-around items-center w-[7.3rem] bg-transparent border-r-[0.1px] border-white/20">
+    <div className="h-full bg-black opacity-90 flex flex-col justify-around items-center w-[7.3rem] border-r-[0.1px] border-white/20 z-40">
       <div className="flex flex-col justify-stretch mb-40 items-center">
         <FaHome className="text-4xl text-white mb-11 mt-10"/>
         <FaHome className="text-4xl text-white mb-11"/>
