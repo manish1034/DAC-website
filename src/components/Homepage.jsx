99fea3e47e74c8f7ff315d778bd13434ea3693e5
manish1034 <76244041+manish1034@users.noreply.github.com>
2023-11-06 03:43:29 +0530

    qwerty

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -24,14 +24,12 @@ const Homepage = () => {
         </div>
         <img src={us} alt="" className="relative h-[100vh] right-0 clip-photo"/>
       </div>
-      <div className="">
-        <div className="absolute top-[30%] left-[19%] bg-white/10 backdrop-blur-lg h-[20.5rem] w-[34rem] rounded-2xl">
-          <h1 className="text-white text-xl m-4">Welcome to</h1>
-          <h1 className="text-white text-5xl mt-2 ml-12 font-sans font-bold">Data Analytics Club, DDUC</h1>
-          <h1 className="text-white text-lg m-4 font-light">where we are dedicated towards nurturing a community of data enthusiasts. Our mission is to equip members with the tools to unravel the potential of data, promoting hands-on learning, ethical analysis, and innovative problem-solving. Join us to explore the realms of data and harness its powers.</h1>
-        </div>
-        <button className="absolute h-8 w-[10rem] bottom-16 left-[20%] bg-[#00fff7] rounded-full font-bold shadow-[#458d8a] shadow-md">Our Journey</button>
+      <div className="absolute top-[30%] left-[19%] bg-white/10 backdrop-blur-lg h-[20.5rem] w-[34rem] rounded-2xl">
+        <h1 className="text-white text-xl m-4">Welcome to</h1>
+        <h1 className="text-white text-5xl mt-2 ml-12 font-sans font-bold">Data Analytics Club, DDUC</h1>
+        <h1 className="text-white text-lg m-4 font-light">where we are dedicated towards nurturing a community of data enthusiasts. Our mission is to equip members with the tools to unravel the potential of data, promoting hands-on learning, ethical analysis, and innovative problem-solving. Join us to explore the realms of data and harness its powers.</h1>
       </div>
+      <button className="absolute h-8 w-[10rem] bottom-16 left-[20%] bg-[#00fff7] rounded-full font-bold shadow-[#458d8a] shadow-md">Our Journey</button>
     </div> 
   )
 }
