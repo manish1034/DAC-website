9a2b4c3339b4676b2acf3cbbcbe3f97c84b564c5
manish1034 <76244041+manish1034@users.noreply.github.com>
2023-12-25 17:25:25 +0530

    nxt committtt

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,7 +14,7 @@ const About = () => {
         {/* DoWnParT DIscRiption */}
         <div className="flex gap-2">
           <div className="flex flex-col h-[34rem] w-[25rem] bg-[#dfe8f6] dark:bg-[#1a222f] rounded-xl mt-10 ml-[10rem] justify-start">
-            <BsPersonCircle className="ml-[7.5rem] mt-9 text-[10rem] bg-[#666666] rounded-full text-[#d9d9d9]"/>
+            <BsPersonCircle className="ml-[7.5rem] mt-9 text-[10rem] bg-[#666666] rounded-full shadow-xl text-[#d9d9d9]"/>
             <h1 className="text-center text-black dark:text-white text-5xl mt-5 font-bold">Name</h1>
             <h1 className="text-center text-black/70 dark:text-white text-2xl mt-0">Position</h1>
             <img src={linkedin} alt="" className="h-7 w-7 ml-[11.5rem] mt-1 text-[2rem] text-[#0177b5] cursor-pointer"/>
@@ -25,7 +25,7 @@ const About = () => {
               {Name.map((ele)=>(
                 <>
                   <div className="flex flex-col justify-center">
-                  <BsPersonCircle className="text-[6rem] bg-[#666666] rounded-full text-[#d9d9d9] cursor-pointer"/>
+                  <BsPersonCircle className="text-[6rem] bg-[#666666] rounded-full text-[#d9d9d9] shadow-md cursor-pointer"/>
                   <h1 className="text-center text-black dark:text-white text-3xl mt-1 font-bold">Name</h1>
                   <h1 className="text-center text-black dark:text-white text-xl mt-0">Position</h1>
                   <img src={linkedin} alt="" className="h-6 w-6 mt-1 ml-9 text-[1.5rem] text-[#0177b5] cursor-pointer"/>
@@ -37,7 +37,7 @@ const About = () => {
               {Name.map((ele)=>(
                   <>
                     <div className="flex flex-col justify-center">
-                    <BsPersonCircle className="text-[6rem] bg-[#666666] rounded-full text-[#d9d9d9] cursor-pointer"/>
+                    <BsPersonCircle className="text-[6rem] bg-[#666666] rounded-full text-[#d9d9d9] shadow-md cursor-pointer"/>
                     <h1 className="text-center text-black dark:text-white text-3xl mt-1 font-bold">Name</h1>
                     <h1 className="text-center text-black dark:text-white text-xl mt-0">Position</h1>
                     <img src={linkedin} alt="" className="h-6 w-6 mt-1 ml-9 text-[1.5rem] text-[#0177b5] cursor-pointer"/>
