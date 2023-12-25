4bd6edaaa407d83d439773dd97511a9fc11f0002
manish1034 <76244041+manish1034@users.noreply.github.com>
2023-12-25 15:21:53 +0530

    commit of the day

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,5 +2,6 @@ import siteBg from "./dashboard-bg-dark.png"
 import darkLogo from "./logo-dark.svg"
 import lightLogo from "./logo-light.svg"
 import us from "./us.png"
+import linkedin from "./Vector.svg"
 
-export { siteBg, darkLogo, lightLogo, us };
+export { siteBg, darkLogo, lightLogo, us, linkedin };
